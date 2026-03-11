8be18c4c4a026208b8e8d9846f3e78140dee8993
Sarwar Hossain <sarwarhridoy4@gmail.com>
2026-03-11 15:02:58 +0600

    Update Prisma to v6 and add auto build-deploy workflow

--- dist/prisma.config.js
+++ dist/prisma.config.js
@@ -1,10 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
+const dotenv_1 = __importDefault(require("dotenv"));
 const config_1 = require("prisma/config");
+dotenv_1.default.config({ path: ".env.local" });
+dotenv_1.default.config();
 exports.default = (0, config_1.defineConfig)({
     schema: "./prisma/schema",
-    datasource: {
-        url: process.env.MONGODB_URI,
-    },
 });
